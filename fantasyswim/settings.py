a78ccc1bd0f75d752fa9a76652f71b3e49a646fd
gtspence <graemespence26@gmail.com>
2016-05-31 16:11:43 +0100

    Adding simple registration and login process

--- fantasyswim/settings.py
+++ fantasyswim/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'rio.apps.RioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Login URL
+LOGIN_URL = '/rio/login/'
