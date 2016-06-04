caf71175b4dcd1532dfd59632a6b22e970abbf19
gtspence <graemespence26@gmail.com>
2016-06-04 15:29:10 +0100

    Add environment variables

--- fantasyswim/settings.py
+++ fantasyswim/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#k_wf-pn%gihu4)(t%^p=+f4(b6nnclejb5z$bb+wik0@z%*a2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,7 +125,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'fantasyswimming@gmail.com'
-EMAIL_HOST_PASSWORD = 'graeme16'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
