58d6621c1535851af7b52d14d95245097e2835e3
Tom Close <tom.close@cantab.net>
2016-06-04 17:10:23 +0100

    Created Team model and migration

--- rio/models.py
+++ rio/models.py
@@ -2,3 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+
+class Team(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=200)
