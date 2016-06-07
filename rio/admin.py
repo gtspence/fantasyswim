949e18811c00e63e898ebbd2b4d2b70d3db1da9e
gtspence <graemespence26@gmail.com>
2016-06-07 13:52:50 +0100

    Add date field to event, change 'nomination' to 'choice', and add more options to admin section

--- rio/admin.py
+++ rio/admin.py
@@ -1,1 +1,33 @@
 from django.contrib import admin
+
+from .models import Team, Event, Swimmer, Participant, Choice
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 1
+
+class ParticipantInline(admin.TabularInline):
+	model = Participant
+	extra = 1
+
+class EventAdmin(admin.ModelAdmin):
+	list_display = ('event', 'date')
+	inlines = [ParticipantInline]
+
+class ParticipantAdmin(admin.ModelAdmin):
+	list_display = ('event', 'swimmer', 'time', 'status')
+	list_filter = ['event']
+
+class SwimmerAdmin(admin.ModelAdmin):
+	list_display = ('name', 'country')
+	list_filter = ['country']
+	inlines = [ParticipantInline]
+
+class TeamAdmin(admin.ModelAdmin):
+	list_display = ('user', 'name')
+	inlines = [ChoiceInline]
+ 
+admin.site.register(Event, EventAdmin)
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Swimmer, SwimmerAdmin)
+admin.site.register(Team, TeamAdmin)
--- rio/models.py
+++ rio/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class Team(models.Model):
     user = models.ForeignKey(User)
@@ -9,6 +10,7 @@ class Team(models.Model):
 
 class Event(models.Model):
 	event = models.CharField(max_length=50)
+	date = models.DateField("Date of Final", null=True, blank=True)
 	
 class Swimmer(models.Model):
 	name = models.CharField(max_length=200)
@@ -21,7 +23,7 @@ class Participant(models.Model):
 	STATUS_CHOICES = (('Y', 'Yes'), ('N', 'No'),)
 	status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='N')
 	
-class Nomination(models.Model):
+class Choice(models.Model):
 	team = models.ForeignKey(Team)
 	event = models.ForeignKey(Event)
 	participant = models.ForeignKey(Participant)
