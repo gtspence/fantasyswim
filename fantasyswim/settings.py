cc12d74191e8a8028a85d5f2012b97ade52aa045
gtspence <graemespence26@gmail.com>
2016-06-19 17:14:08 +0100

    Edit settings for SendGrid (wasn't working)

--- fantasyswim/settings.py
+++ fantasyswim/settings.py
@@ -126,8 +126,8 @@ LOGIN_REDIRECT_URL = '/rio/'
 
 # SendGrid email settings
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'sendgrid_username'
-EMAIL_HOST_PASSWORD = 'sendgrid_password'
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
