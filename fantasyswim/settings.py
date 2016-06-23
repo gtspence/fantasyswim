462239fa06d532a959977aeed9619869e4f7a47f
gtspence <graemespence26@gmail.com>
2016-06-23 12:09:58 +0100

    Correct mistake in settings(?) - take 2

--- fantasyswim/settings.py
+++ fantasyswim/settings.py
@@ -154,9 +154,9 @@ LOGIN_URL = '/rio/login/'
 LOGIN_REDIRECT_URL = '/rio/'
 
 # SendGrid email settings
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'fantasyswimming@gmail.com'
